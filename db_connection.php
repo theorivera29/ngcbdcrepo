c84ff49c8250c7798fbaad10f7063191b8317332
Theo Rivera <theorivera29.tmk@gmail.com>
2019-06-25 13:13:09 +0800

    Files

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
     $host = 'localhost';
-	$user = 'u627900414_ngcbd';
-	$pass = 'ngcbdc';
-	$db = 'u627900414_ngcbd';
+	$user = 'root';
+	$pass = '';
+	$db = 'ngcbdc';
 	$conn = mysqli_connect($host, $user, $pass, $db) or die('Cannot connect to db');
 	date_default_timezone_set('Asia/Manila');
 ?>
