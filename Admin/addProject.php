2af8bff92731713b241b427aad4594471a850759
Caryl Marie <2161397@slu.edu.ph>
2019-06-29 23:03:16 +0800

    Update addProject.php
    updated validation in add project, fixed the js for the start and end date

--- Admin/addProject.php
+++ Admin/addProject.php
@@ -12,18 +12,27 @@
     <title>NGCBDC</title>
     <link rel="icon" type="image/png" href="../Images/login2.png">
     <link rel="stylesheet" type="text/css" href="../style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.js"
+        integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 </head>
 
 <body>
-<div id="content">
+    <div id="content">
         <span class="slide">
             <a href="#" class="open" onclick="window.location.href='projects.php'">
                 <i class="fas fa-arrow-circle-left"></i>
@@ -54,21 +63,29 @@
         <form action="../server.php" method="POST" class="needs-validation" novalidate>
             <div class="form-group">
                 <label for="projectName" class="label-styles">PROJECT NAME:</label>
-                <input name="projectName" id="projectName" type="text" class="form-control" placeholder="Enter project name" title="Input letters" pattern="^[A-Za-z][A-Za-z0-9\s-_#&.()@/<>';:,|]*$"  required>
+                <input name="projectName" id="projectName" type="text" class="form-control"
+                    placeholder="Enter project name" title="Input letters"
+                    pattern="^[A-Za-z][A-Za-z0-9\s-_#&.()@/<>';:,|]*$" required>
+                <div class="invalid-feedback">Please fill out this field.</div>
             </div>
             <div class="form-group">
                 <label for="address" class="label-styles">ADDRESS:</label>
-                <input name="address" id="address" type="text" class="form-control" placeholder="Enter project address" title="Input letters and numbers only" pattern="^[A-Za-z][A-Za-z0-9\s.,-@#^&*]*$" required>
+                <input name="address" id="address" type="text" class="form-control" placeholder="Enter project address"
+                    title="Input letters and numbers only" pattern="^[A-Za-z][A-Za-z0-9\s.,-@#^&*]*$" required>
+                <div class="invalid-feedback">Please fill out this field.</div>
             </div>
             <div class="form-group row">
                 <div class="col-lg-6">
                     <label for="startDate" class="label-styles">START DATE:</label>
                     <input name="startDate" id="startDate" type="date" class="form-control start-date"
-                        onchange="startDateEnable()" required>
+                        onkeydown="return false" onchange="startDateEnable()" required>
+                    <div class="invalid-feedback">Please choose a start date.</div>
                 </div>
                 <div class="col-lg-6">
                     <label for="endDate" class="label-styles">END DATE:</label>
-                    <input name="endDate" id="endDate" type="date" class="form-control end-date" required>
+                    <input name="endDate" id="endDate" type="date" class="form-control end-date"
+                        min="$('#startdate').val()" onkeydown="return false" disabled required>
+                    <div class="invalid-feedback">Please choose an end date.</div>
                 </div>
             </div>
             <label class="label-styles">Materials Engineer Involved</label>
@@ -79,26 +96,25 @@
                     while($rowmateng = mysqli_fetch_row($resultmateng)){
                 ?>
 
-            <div class="input-group mb-2 col-md-12">
-                <div class="input-group-prepend col-md-12 options">
-                    <div class="input-group-text">
+             <div class="custom-control custom-checkbox mb-3 options">
+                
+                <input type="checkbox" class="custom-control-input" name="mateng[]" id="selectMatEng-<?php echo $rowmateng[1]?>" value="<?php echo $rowmateng[1]?>" required>
+                <label class="custom-control-label" for="selectMatEng-<?php echo $rowmateng[1]?>"><?php echo $rowmateng[0]?></label>            
+            </div> 
+            <div class="invalid-feedback">Please select atleast one Materials Engineer.</div>
 
-                        <input type="checkbox" name="mateng[]" aria-label="Checkbox for following text input" value="<?php echo $rowmateng[1]?>" required>
-                    </div>
-
-                    <input type="text" class="form-control" aria-label="Text input with checkbox" value="<?php echo $rowmateng[0]?>" disabled>
-                </div>
-            </div>
+           
             <?php
                 }
             ?>
             <div class="add-project-btn">
-                <button type="submit" class="btn btn-success add-proj">Save</button>
-                <input type="reset" class="btn btn-danger" value="Cancel">
+                <button type="submit" class="btn btn-success add-proj">Save Project</button>
+                <input type="reset" class="btn btn-danger" value="Clear">
             </div>
 
             <!-- Start of confirmation modal -->
-            <div class="modal fade" id="create-proj-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="create-proj-modal" tabindex="-1" role="dialog"
+                aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -122,9 +138,9 @@
 </body>
 
 <script type="text/javascript">
-    $(document).ready(function() {
+    $(document).ready(function () {
 
-        $('#sidebarCollapse').on('click', function() {
+        $('#sidebarCollapse').on('click', function () {
             $('#sidebar').toggleClass('active');
         });
     });
@@ -139,6 +155,11 @@
     }
 
     function startDateEnable() {
+        if (document.getElementById("startDate").value === "") {
+            document.getElementById('endDate').disabled = true;
+        } else {
+            document.getElementById('endDate').disabled = false;
+        }
         var date = $("#startDate").val();
         var dtToday = new Date(date);
 
@@ -154,15 +175,26 @@
         $('#endDate').attr('min', start);
     }
 
-    $(document).ready(function() {
-        $(".add-proj").click(function(e) {
+    $(function () {
+        var requiredCheckboxes = $('.options :checkbox[required]');
+        requiredCheckboxes.change(function () {
+            if (requiredCheckboxes.is(':checked')) {
+                requiredCheckboxes.removeAttr('required');
+            } else {
+                requiredCheckboxes.attr('required', 'required');
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $(".add-proj").click(function (e) {
             var projname = $("#projectName").val();
             var address = $("#address").val();
             var sdate = $("#startDate").val();
             var edate = $("#endDate").val();
-            var selectMat = $("#mateng option:selected").val();
-            if ((projname != '') && (address != '') && (sdate != '') && (edate != '') && (selectMat !=
-                    '')) {
+            var selectMat = $("input:checkbox[name='mateng[]']");
+            if ((projname != '') && (address != '') && (sdate != '') && (edate != '') && (selectMat.is(
+                    ":checked"))) {
                 e.preventDefault();
                 $("#create-proj-modal").modal('show');
             }
@@ -185,21 +217,8 @@
         }, false);
     })();
 
-    $(function () {
-        var requiredCheckboxes = $('.options :checkbox[required]');
-        requiredCheckboxes.change(function () {
-            if (requiredCheckboxes.is(':checked')) {
-                requiredCheckboxes.removeAttr('required');
-            } else {
-                requiredCheckboxes.attr('required', 'required');
-            }
-        });
-    });
+  
 
-    bootstrapValidate('#projectName', 'regex:^[A-Za-z][A-Za-z0-9\s-_#&.+()@/<>`~|]*$:You can only input alphabetic characters.')
-    bootstrapValidate('#address', 'required:Please fill out this field!')
-    bootstrapValidate('#startDate', 'required:Please fill out this field!')
-    bootstrapValidate('#endDate', 'required:Please fill out this field!')
 </script>
 
 </html>
