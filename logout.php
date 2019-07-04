d3ac6f5b9cc31821bd222b157287df9cdd2300e3
Caryl Marie <2161397@slu.edu.ph>
2019-07-04 17:25:58 +0800

    Revert "View Inventory Navigation problem"
    This reverts commit 26ef3b653d802ef785fa99ea95ca809856dc8096.

--- logout.php
+++ logout.php
@@ -2,5 +2,5 @@
     session_start();
     session_unset();
     session_destroy();
-    header('location: https://inventory-system-ngbdc.hostingerapp.com/index.php');
+    header('location: http://localhost/ngcbdcrepo/index.php');
 ?>
